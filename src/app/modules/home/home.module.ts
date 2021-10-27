c4f14a0e705e64276f80f1c8cf7b210c5ff0259e
Ángel Cruz <langelcruzlara@gmail.com>
2021-10-27 12:45:04 -0500

    feat: adding sidebar

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { SharedModule } from '@shared/shared.module';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
-
 @NgModule({
-  declarations: [
-    HomePageComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomePageComponent],
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
-export class HomeModule { }
+export class HomeModule {}
