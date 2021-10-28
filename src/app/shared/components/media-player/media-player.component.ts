54f4b849d67055a145d54e65c35dc8b83bd4a8a3
Ángel Cruz <langelcruzlara@gmail.com>
2021-10-27 22:49:04 -0500

    feat: adding shared components

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TrackModel } from '@core/models/tracks.model';
+
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
-  styleUrls: ['./media-player.component.scss']
+  styleUrls: ['./media-player.component.scss'],
 })
 export class MediaPlayerComponent implements OnInit {
+  mockCover: TrackModel = {
+    cover: 'daf',
+    album: 'test',
+    name: '',
+    url: '',
+    _id: 7,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,24 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MediaPlayerComponent } from './components/media-player/media-player.component';
 import { HeaderUserComponent } from './components/header-user/header-user.component';
+import { CardPlayerComponent } from './components/card-player/card-player.component';
+import { GenericSectionComponent } from './components/generic-section/generic-section.component';
 
 @NgModule({
-  declarations: [SidebarComponent, MediaPlayerComponent, HeaderUserComponent],
+  declarations: [
+    SidebarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent,
+    CardPlayerComponent,
+    GenericSectionComponent,
+  ],
   imports: [CommonModule],
-  exports: [SidebarComponent, MediaPlayerComponent, HeaderUserComponent],
+  exports: [
+    SidebarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent,
+    CardPlayerComponent,
+    GenericSectionComponent,
+  ],
 })
 export class SharedModule {}
