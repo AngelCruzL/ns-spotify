f3adc59eac8a3ac8b65823d44e5c215bf18c933b
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-24 19:30:22 -0600

    fix: sidebar styles and remove unused function

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -14,7 +13,7 @@ export class SidebarComponent implements OnInit {
 
   customOptions: Array<any> = [];
 
-  constructor(private router: Router) {}
+  constructor() {}
 
   ngOnInit(): void {
     this.mainMenu.defaultOptions = [
@@ -65,10 +64,5 @@ export class SidebarComponent implements OnInit {
         router: ['/'],
       },
     ];
-  }
-
-  goTo($event: any): void {
-    this.router.navigate($event.router);
-    console.log($event);
   }
 }
