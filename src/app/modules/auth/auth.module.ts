b190c4b38d5f599be6638e79c1af8cf8bc8fcf29
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-24 20:40:14 -0600

    fix: rename auth-page to login component

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { AuthPageComponent } from './pages/auth-page/auth-page.component';
-
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [
-    AuthPageComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [LoginComponent],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
-export class AuthModule { }
+export class AuthModule {}
