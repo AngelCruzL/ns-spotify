2a98ee37d2331728b1260cf723af937765570810
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-03-04 17:38:42 -0600

    feat: add functionality to get and display the elapsed and remaining time

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -8,14 +8,46 @@ import { TrackModel } from '@core/models/tracks.model';
 })
 export class MultimediaService {
   trackInfo$: BehaviorSubject<any> = new BehaviorSubject<any>(undefined);
+  timeElapsed$: BehaviorSubject<string> = new BehaviorSubject<string>('00:00');
+  timeRemaining$: BehaviorSubject<string> = new BehaviorSubject<string>('-00:00');
   audio!: HTMLAudioElement;
 
   constructor() {
     this.audio = new Audio();
     this.trackInfo$.subscribe((track: TrackModel) => {
-      console.log('track', track)
       if (track) this.setAudio(track);
+
+      this.#listenAllEvents();
     });
+  }
+
+  #listenAllEvents(): void {
+    this.audio.addEventListener('timeupdate', this.#calculateTime, false);
+  }
+
+  #calculateTime = (): void => {
+    const { duration, currentTime } = this.audio;
+    this.#setTimeElapsed(currentTime);
+    this.#setTimeRemaining(currentTime, duration);
+  };
+
+  #setTimeElapsed(currentTime: number): void {
+    const minutes = Math.floor((currentTime / 60) % 60);
+    const seconds = Math.floor(currentTime % 60);
+    const displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+    const result = `${displayMinutes}:${displaySeconds}`;
+    this.timeElapsed$.next(result);
+  }
+
+  #setTimeRemaining(currentTime: number, duration:number): void {
+    let timeRemaining = duration - currentTime;
+    const minutes = Math.floor((timeRemaining / 60) % 60);
+    const seconds = Math.floor(timeRemaining % 60);
+    const displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+    const result = `-${displayMinutes}:${displaySeconds}`;
+    this.timeRemaining$.next(result);
   }
 
   setAudio(track: TrackModel): void {
