e62763d3d440abdf24f4ab546bcc62c104bf77a1
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-08-13 10:25:53 -0600

    Add form login component test cases

--- src/app/modules/auth/pages/login/login.component.spec.ts
+++ src/app/modules/auth/pages/login/login.component.spec.ts
@@ -3,6 +3,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { By } from '@angular/platform-browser';
+
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -27,7 +29,38 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should return "invalid" when the form don\'t pass the validation', () => {
+    const mockCredentials = {
+      email: 'this is not an email',
+      password: '123'
+    };
+    const emailFormValue = component.loginForm.get('email');
+    const passwordFormValue = component.loginForm.get('password');
+
+    emailFormValue?.setValue(mockCredentials.email);
+    passwordFormValue?.setValue(mockCredentials.password);
+
+    expect(component.loginForm.invalid).toBeTruthy();
+  });
+
+  it('should return "valid" when the form pass the validation', () => {
+    const mockCredentials = {
+      email: 'me@angelcruzl.dev',
+      password: '123456'
+    };
+    const emailFormValue = component.loginForm.get('email');
+    const passwordFormValue = component.loginForm.get('password');
+
+    emailFormValue?.setValue(mockCredentials.email);
+    passwordFormValue?.setValue(mockCredentials.password);
+
+    expect(component.loginForm.valid).toBeTruthy();
+  });
+
+  it('should render the login button with the text "Iniciar sesión"', () => {
+    const elementRef = fixture.debugElement.query(By.css('.form-action button'));
+    const innerText = elementRef.nativeElement.innerText;
+
+    expect(innerText).toEqual('Iniciar sesión');
   });
 });
