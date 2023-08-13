d8ea4b99fa126638064c11b736d3f43b786c2d70
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-08-13 15:48:08 -0600

    Remove modules and update router files

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,32 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { InjectSessionInterceptor } from '@core/interceptors/inject-session.interceptor';
-import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app/app.routes';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule),
-        CookieService,
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: InjectSessionInterceptor,
-            multi: true,
-        },
-        provideHttpClient(withInterceptorsFromDi()),
-    ]
-})
-  .catch(err => console.error(err));
+  providers: [
+    provideRouter(appRoutes),
+    importProvidersFrom(BrowserModule),
+    CookieService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InjectSessionInterceptor,
+      multi: true,
+    },
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+}).catch(err => console.error(err));
