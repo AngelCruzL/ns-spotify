7eba44d566b613954241a6913af4bed21ad1ffe7
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-08-13 19:47:37 -0600

    Add required input and alias example

--- src/app/shared/components/card-player/card-player.component.ts
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -3,22 +3,18 @@ import { Component, Input, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 import { MultimediaService } from '@shared/services/multimedia.service';
 import { ImgBrokenDirective } from '../../directives/img-broken.directive';
-import { NgIf, NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
-    selector: 'app-card-player',
-    templateUrl: './card-player.component.html',
-    styleUrls: ['./card-player.component.scss'],
-    standalone: true,
-    imports: [
-        NgIf,
-        NgClass,
-        ImgBrokenDirective,
-    ],
+  selector: 'app-card-player',
+  templateUrl: './card-player.component.html',
+  styleUrls: ['./card-player.component.scss'],
+  standalone: true,
+  imports: [NgIf, NgClass, ImgBrokenDirective],
 })
 export class CardPlayerComponent implements OnInit {
-  @Input() mode: 'small' | 'big' = 'small';
-  @Input() track: TrackModel = {
+  @Input({ required: true }) mode: 'small' | 'big' = 'small';
+  @Input({ required: true, alias: 'trackObject' }) track: TrackModel = {
     _id: 0,
     album: '',
     cover: '',
