1bfea6b894495607d0bf5d3a73d8ed90b2f74691
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-08-13 19:19:50 -0600

    Add custom untilDestroy implementation

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,34 +1,33 @@
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MultimediaService } from '@shared/services/multimedia.service';
-import { Subscription } from 'rxjs';
 import { ImgBrokenDirective } from '../../directives/img-broken.directive';
-import { NgTemplateOutlet, NgIf, NgClass, AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgClass, NgIf, NgTemplateOutlet } from '@angular/common';
+import { customUntilDestroy } from '@core/utils/custom-until-destroy';
 
 @Component({
-    selector: 'app-media-player',
-    templateUrl: './media-player.component.html',
-    styleUrls: ['./media-player.component.scss'],
-    standalone: true,
-    imports: [NgTemplateOutlet, NgIf, ImgBrokenDirective, NgClass, AsyncPipe]
+  selector: 'app-media-player',
+  templateUrl: './media-player.component.html',
+  styleUrls: ['./media-player.component.scss'],
+  standalone: true,
+  imports: [NgTemplateOutlet, NgIf, ImgBrokenDirective, NgClass, AsyncPipe],
 })
-export class MediaPlayerComponent implements OnInit, OnDestroy {
+export class MediaPlayerComponent implements OnInit {
+  multimediaService = inject(MultimediaService);
+  customUntilDestroy = customUntilDestroy();
+
   @ViewChild('progressBar') progressBar: ElementRef = new ElementRef('');
-  observersList$: Array<Subscription> = [];
   state: string = 'paused';
 
-  constructor(public multimediaService: MultimediaService) {
-  }
-
   ngOnInit(): void {
-    const observer1$ = this.multimediaService.playerStatus$.subscribe((state: string) => {
-      this.state = state;
-    });
-
-    this.observersList$.push(observer1$);
-  }
-
-  ngOnDestroy(): void {
-    this.observersList$.forEach((observer) => observer.unsubscribe());
+    this.multimediaService.playerStatus$
+      .pipe(this.customUntilDestroy())
+      .subscribe((state: string) => (this.state = state));
   }
 
   handlePosition(event: MouseEvent) {
